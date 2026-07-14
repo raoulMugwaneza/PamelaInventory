112a3f38d4c65f77555e2d34d77c39c096180fb5
raoul <raoulmugwaneza@gmail.com>
2026-07-14 19:34:59 +0200

    Day 3: routes split into router files, mounted with app.use

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const greetRouter = require('./routes/greet'); 
+const searchRouter = require('./routes/search'); 
+
+app.use('/search', searchRouter); 
+app.use('/greet', greetRouter); 
+
+app.get('/', (req, res) => {
+    res.send('Pamoja says hello')
+});
 
 app.listen(3001, () => {
     console.log('Pamoja server running on port 3001')
 }); 
+
+
